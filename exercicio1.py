240904e47c4817e5119bdd01fb4a6808cf5d5e32
Lavouisier Nogueira <lavouisier@gmail.com>
2026-08-20 23:01:49 -0300

    Primeiro exercício

---
+++ exercicio1.py
@@ -0,0 +1,15 @@
+nome = 'Carlos'
+sobrenome = 'Silva'
+idade = 26
+ano_nascimento = 2026 - idade
+maior_de_idade = idade >= 18
+altura_metros = 1.85
+
+
+
+print(f'Nome: {nome}')
+print(f'Sobrenome: {sobrenome}')
+print(f'Idade: {idade}')
+print(f'Ano de nascimento: {ano_nascimento}')
+print(f'Maior de idade? {maior_de_idade}')
+print(f'Altura em metros: {altura_metros}')
