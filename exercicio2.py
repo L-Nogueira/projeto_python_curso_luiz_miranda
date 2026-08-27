dc809aca0ffab80552907d5efe2caa8ff53d9b2c
Lavouisier Nogueira <lavouisier@gmail.com>
2026-08-26 23:59:44 -0300

    Adição de detalhamento sobre as formas de formatação das f-strings

--- exercicio2.py
+++ exercicio2.py
@@ -10,3 +10,22 @@ bmi = weigh / (heigh**2)
 print(f'{name} is {heigh:.2f} m tall,')
 print(f'He has {weigh} kg')
 print(f'and his BMI is: {bmi:.2f}')
+
+'''
+A formatação com f-string possúi uma sintaxe própria que permite definir
+elementos para preenchimento de espaçõs.
+O alinhamento (< ^ >), a largura do espaçamento, o separador (. , _), 
+a precisão e o tipo de dado.
+
+Mas só funciona se estiver na órdem certa 
+:[preenchimento][alinhamento][largura][separador][.precisão][tipo]
+
+Exemplo usando preenchimento, alinhamento, largura, separador, precisão e tipo
+
+itens = [('Processador', 1200.0), ('RAM 16GB', 700.25), ('SSD 1TB', 500.50)]
+
+print(f'{'Item':-<20}{'Preço':->12}')
+print('-' * 32)
+for item, preco in itens:
+    print(f'{item:.<20}R${preco:>9,.2f}')
+'''
